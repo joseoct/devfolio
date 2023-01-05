ee779342796d6f1a35e50d5f94db1e64cb4064f1
Jose <joseoctavioconti@gmail.com>
2023-01-05 19:30:27 -0300

    setup for changing to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,119 +1,34 @@
-import { Box, Fade, Flex, HStack, Image, Link, Text, Tooltip, VStack } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import { assets } from './assets';
-import { DencryptTextEffect } from './components/DencryptTextEffect';
-import { Header } from './components/Header';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
 function App() {
-
-  const [showContent, setShowContent] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      return (
-        setShowContent(true)
-      )
-    }, 0)
-  }, [])
+  const [count, setCount] = useState(0)
 
   return (
-    <Flex direction="column">
-      <Fade in={showContent}>
-        <Header />
-      </Fade>
-
-      <Flex bg="blackAlpha.600" h="100vh" alignItems="center" justifyContent="center">
-        <VStack align="start">
-          <DencryptTextEffect
-            inverted
-            fontSize={['3xl']}
-            values={['José Octávio']}
-            time={0}
-          />
-          <DencryptTextEffect
-            fontSize={['6xl', '6xl', '8xl']}
-            values={['Full Stack']}
-            time={1500}
-          />
-          <DencryptTextEffect
-            fontSize={['6xl', '5xl', '8xl']}
-            values={['Developer']}
-            time={1500}
-          />
-          <DencryptTextEffect
-            fontSize={['xl', '2xl']}
-            values={['NodeJS', 'ReactJS', 'React-Native']}
-            time={1500}
-          />
-        </VStack>
-      </Flex>
-
-      {
-        showContent && (
-          <Flex
-            bg="blackAlpha.500"
-            id="about-me"
-            h="100vh"
-            alignItems="center"
-            justifyContent="center"
-          >
-
-            <Flex maxWidth={800} gap={8} alignItems="center" justifyContent="center" flexDir={['column', 'column', 'row']} margin={8}>
-              <Image src={"https://avatars.githubusercontent.com/u/40551163?v=4"} rounded="full" boxSize={['200', '200', '300']} />
-              <Box>
-                <Text fontSize="lg" textAlign="center">
-                  Born in Brazil, 24 years old, bachelor's degree in Computer Science from the
-                  <Link href="http://www.utfpr.edu.br/" target="_blank" color="orange.300" style={{ textDecoration: "none" }}> Federal Technological University of Paraná.</Link>
-                </Text>
-                <Text as="p" fontSize="lg" textAlign="center">
-                  I love back-end, independent of the programming language, and I am currently deepening into front-end.
-                  I feel fulfilled when I finish a project and see things working, and I realized that it is not a programming
-                  language that forms a programmer, but his logic and his capacity to learn new things.
-                </Text>
-                <br />
-                <Flex justifyContent="center" alignItems="center" flexDirection="row" gap={2} flexDir="row">
-                  <Text fontSize="small" textAlign="center" >
-                    Currently, my development environment is:
-                  </Text>
-                  <Flex gap={1}>
-                    <Tooltip label={assets[6].name}>
-                      <Image src={assets[6].icon} boxSize={6} />
-                    </Tooltip>
-                    <Tooltip label={assets[7].name}>
-                      <Image src={assets[7].icon} boxSize={6} />
-                    </Tooltip>
-                  </Flex>
-                </Flex>
-              </Box>
-            </Flex>
-          </Flex>
-        )
-      }
-
-      {
-        showContent && (
-          <Flex
-            bg="blackAlpha.400"
-            id="about-me"
-            h="100vh"
-            alignItems="center"
-            justifyContent="center"
-          >
-            {assets.map((asset, index) => (
-              <Box key={index}>
-                <Tooltip label={asset.name}>
-                  <Image
-                    src={asset.icon}
-                    alt="logo"
-                  />
-                </Tooltip>
-              </Box>
-            ))}
-          </Flex>
-        )
-      }
-    </Flex >
-  );
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
 
 export default App
